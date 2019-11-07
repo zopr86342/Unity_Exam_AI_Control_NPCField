cac669adc94d9e899d61b3abe7ecc0a70afd13b3
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2019-11-07 11:51:39 +0800

    期中

---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour {
+    [Header("對話")]
+    public string say = "給我電池!!!";
+    public float SaySpeed = 0.2f;
+    [Header("任務")]
+    public bool Complete = false;
+    public int BaetryFinish = 5;
+    public int Baetry = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/robot_ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot_ball : MonoBehaviour {
+    [Range(0, 1000), Header("速度")]
+    public int speed=50;
+    [Range(0, 1000), Header("跳躍")]
+    public int jump=100;
+    [Header("是否在地上")]
+    public bool IsGround = true;
+    Rigidbody ball;
+	// Use this for initialization
+	void Start () {
+        ball = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGround = true;
+    }
+    private void FixedUpdate()
+    {
+        Jump();
+        Walk();
+
+    }
+    private void Walk()
+    {
+        ball.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical")));
+    }
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&&IsGround==true)
+        {
+            IsGround = false;
+            ball.AddForce(new Vector3(0, jump, 0));
+        }
+    }
+    }
+
+
